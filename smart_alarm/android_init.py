198285933c588e838d7b122deb07faa490418168
jduo <jduo@juju.com>
2020-12-27 13:14:20 -0300

    Several improvements on deployment

--- smart_alarm/android_init.py
+++ smart_alarm/android_init.py
@@ -1,9 +1,13 @@
 import androidhelper
 import logging
+import socket
 
 try:
     droid = androidhelper.Android()
     print(droid)
-except:
+    print('SL4A: Connection OK')
+except socket.error as e:
     logging.basicConfig()
     logging.exception('While testing SL4A')
+    if str(e) == '[Errno 111] Connection refused':
+        print('SL4A: Connection refused')
