0a6968591edbdc33a7bd26c3e0719feec06ec4f4
jduo <jduo@juju.com>
2021-01-09 17:54:03 -0300

    Add cmds_server, WIP

---
+++ smart_alarm/cmds_server.py
@@ -0,0 +1,150 @@
+"""
+smart_alarm
+Copyright (c) 2020, Joaquin G. Duo
+
+Code Licensed under LGPL License. See LICENSE file.
+"""
+from typing import Optional
+from fastapi import Depends, HTTPException, status
+from pydantic import BaseModel
+import os
+import logging
+from smart_alarm.cmds_server_helper import User, get_current_active_user, app,\
+    AndroidRPC
+from fastapi_utils.tasks import repeat_every
+import json
+
+logger = logging.getLogger('cmds_server')
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+
+def split_phones(phones_str):
+    return [p.strip() for p in phones_str.split(',') if p.strip()]
+
+SMS_CHECK_SECONDS=5
+LOCAL_PHONES_PREFIX = os.environ.get('LOCAL_PHONES_PREFIX','')
+ALARM_NOTIFIED_PHONES = split_phones(os.environ.get('ALARM_NOTIFIED_PHONES', ''))
+ALARM_ADMIN_PHONES = split_phones(os.environ.get('ALARM_ADMIN_PHONES', ''))
+CMDS_AUTH_TOKEN= os.environ.get('CMDS_AUTH_TOKEN')
+assert CMDS_AUTH_TOKEN
+
+
+class AlarmCfg:
+    notified_phones = ALARM_NOTIFIED_PHONES
+    admin_phones = ALARM_ADMIN_PHONES
+    enabled = False
+    sms_check_seconds = SMS_CHECK_SECONDS
+    reply_msgs = True
+
+
+counter = 0
+sms_rcv = dict(msgs=[], ids=set())
+@app.on_event("startup")
+@repeat_every(seconds=SMS_CHECK_SECONDS, logger=logger, wait_first=True)
+def periodic():
+    msgs = AndroidRPC().sms_get_messages(unread=True)
+    if msgs.get('error'):
+        logger.error(f'While checking incoming SMS: {msgs}')
+        return
+    new_messages = set()
+    for m in msgs['result']['rpc_result']['result']:
+        if m['_id'] not in sms_rcv['ids']:
+            new_messages.add(m['_id'])
+            sms_rcv['ids'].add(m['_id'])
+            sms_rcv['msgs'].append(m)
+            process_cmd(m)
+    if new_messages:
+        AndroidRPC().smsMarkMessageRead(list(new_messages),True)
+        with open('msgs.json', 'w') as fp:
+            json.dump(sms_rcv['msgs'], fp, indent=1)
+
+
+def process_cmd(msg):
+    # msg = {'read': '0', 'body': 'Tes',     '_id': '4', 'date': '1610213710004', 'address': '+1...'}
+    cmd = msg.get('body')
+    cmd = cmd.upper()
+    if msg.get('address') in AlarmCfg.admin_phones:
+        if cmd.startswith('ON'):
+            AlarmCfg.enabled = True
+            logging.info('Enabling alarm')
+            reply_msg(msg, 'Alarm Enabled')
+        elif cmd.startswith('OFF'):
+            AlarmCfg.enabled = False
+            logging.info('Disabling alarm')
+            reply_msg(msg, 'Alarm Disabled')
+        elif cmd.startswith('STATUS'):
+            # TODO: check if NVR + cameras are online + etc
+            logging.info('Sending status')
+            reply_msg(msg, build_status())
+    else:
+        print(msg)
+
+
+def build_status():
+    def removeprefix(s, pref):
+        if s.startswith(pref):
+            return s[len(pref):]
+        return s
+    admin = ','.join(removeprefix(p, LOCAL_PHONES_PREFIX) for p in AlarmCfg.admin_phones)
+    notify = ','.join(removeprefix(p, LOCAL_PHONES_PREFIX) for p in AlarmCfg.notified_phones)
+    return f'Enabled:{AlarmCfg.enabled} Admins:{admin} Notify:{notify}'
+
+
+def reply_msg(msg, reply_body):
+    if AlarmCfg.reply_msgs:
+        AndroidRPC().sms_send(msg['address'], reply_body)
+
+
+class Msg(BaseModel):
+    msg: str
+    msg_from: Optional[str] = None
+    msg_type: Optional[str] = None
+    auth_token: str
+
+
+notifications_recv = []
+@app.post("/alarm/notification/")
+async def alarm_notification(msg: Msg):
+    if msg.auth_token != CMDS_AUTH_TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect request token",
+        )
+    results = []
+    errors = []
+    notifications_recv.append(msg)
+    if not AlarmCfg.enabled:
+        return dict(result='alarm disabled')
+    for p in AlarmCfg.notified_phones:
+        r = AndroidRPC().sms_send(p, msg.msg)
+        if r.get('error'):
+            errors.append(r)
+        else:
+            results.append(r)
+        if 'This is a test mail send by your NVR' in msg:
+            break
+    msg.auth_token = None
+    return dict(result=results, error=errors)
+
+
+@app.get("/alarm/notifications/")
+async def alarm_notifications(current_user: User = Depends(get_current_active_user)):
+    return dict(notifications=[m.msg for m in notifications_recv],
+                sms=[m['body'] for m in sms_rcv['msgs']])
+
+
+@app.post("/alarm/activate/")
+async def alarm_activate(current_user: User = Depends(get_current_active_user)):
+    return dict(result='activated')
+
+
+@app.get("/alarm/status/")
+async def alarm_status(current_user: User = Depends(get_current_active_user)):
+    return dict(result='activated')
+
+
+@app.post("/alarm/reset/")
+async def alarm_reset(current_user: User = Depends(get_current_active_user)):
+    return dict(result='resetted')
+
---
+++ smart_alarm/cmds_server_helper.py
@@ -0,0 +1,192 @@
+"""
+smart_alarm
+Copyright (c) 2020, Joaquin G. Duo
+
+Code Licensed under LGPL License. See LICENSE file.
+"""
+from datetime import datetime, timedelta
+from typing import Optional
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi import Depends, HTTPException, status, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+import os
+import logging
+import requests
+from fastapi.security import OAuth2PasswordRequestForm
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRE_MINUTES', '1200'))
+ANDROID_SERVER = os.environ.get('ANDROID_SERVER', '127.0.0.1')
+ANDROID_SERVER_PORT = int(os.environ.get('ANDROID_SERVER_PORT', '8001'))
+ANDROID_AUTH_TOKEN= os.environ.get('ANDROID_AUTH_TOKEN')
+
+assert SECRET_KEY and ANDROID_AUTH_TOKEN
+
+logger = logging.getLogger('cmds_server_helper')
+
+def get_db():
+    USER_HASHED_PASSWORD = os.environ.get('USER_HASHED_PASSWORD')
+    assert USER_HASHED_PASSWORD
+    fake_users_db = {
+        "salarm": {
+            "username": "salarm",
+            "hashed_password": USER_HASHED_PASSWORD,
+            "disabled": False,
+        }
+    }
+    return fake_users_db
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(get_db(), username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://null.jsbin.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(get_db(), form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+class AndroidRPC:
+    def __getattr__(self, name):
+        def method(*args, **kwargs):
+            r = self.do_rpc(name, *args, **kwargs)
+            if r.get('error'):
+                logger.error(f'While calling AndroidRPC.{name}(*{args},**{kwargs}) {r}')
+            return r
+        return method
+
+    def do_rpc(self, method, *args, **kwargs):
+        url = f'http://{ANDROID_SERVER}:{ANDROID_SERVER_PORT}' 
+        json = {'method': method,
+                'args': args,
+                'kwargs':kwargs,
+                'auth_token': ANDROID_AUTH_TOKEN}
+        try:
+            r = requests.post(url, json=json)
+            logger.debug(f'Got {r.json()} for {method!r} {args} {kwargs}')
+            return dict(result=r.json())
+        except Exception as e:
+            logger.exception(f'While RPC {method!r} {args} {kwargs}')
+            return dict(error=f'RPC {method!r} {args} {kwargs} {e} {e!r}')
+
+    def sms_send(self, phone, msg):
+        return self.do_rpc('sms_send', phone=phone, msg=msg)
+
+    def sms_get_messages(self, unread=True):
+        return self.do_rpc('sms_get_messages', unread=unread)
